03adbf2fa82464d057135f869eb3d16231cb710b
Elina <elina@MacBook-Pro-Elina.local>
2025-10-05 01:07:48 +0300

    develop

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,73 @@
 'use strict';
 
-// write code here
+// function sortList(list) {
+//     const data = document.querySelector('data-salary')
+//     const newList = [...list].map((data) =>
+//     data.dataset.selery.replace('$', '').replace(',', ''),
+// );
+// let counter = 0;
+
+// for (let i = 0; i < newList.length; i++) {
+//     if (Number(newList[i + 1]) <= Number(newList[i])) {
+//         counter++
+//     }
+// }
+//   function getEmployees(list) {
+//     return newList;
+//   }
+// }
+
+// getEmployees(newList);
+// sortList(newList);
+
+//
+
+'use strict';
+
+const parseSalary = (salaryStr) => {
+  return Number(salaryStr.replace('$', '').replace(',', ''));
+};
+
+function sortList(list) {
+  if (!list || !list.children.length) {
+    return;
+  }
+
+  const listItems = Array.from(list.children);
+
+  listItems.sort((a, b) => {
+    const salaryA = parseSalary(a.dataset.salary);
+    const salaryB = parseSalary(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  listItems.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(list) {
+  if (!list || !list.children.length) {
+    return [];
+  }
+
+  const currentListItems = Array.from(list.children);
+
+  const employers = currentListItems.map((item) => {
+    const nameN = item.dataset.name;
+    const salary = parseSalary(item.dataset.salary);
+
+    return {
+      name: nameN,
+      salary: salary,
+    };
+  });
+
+  return employers;
+}
+
+const employeeList = document.querySelector('ul');
+
+if (employeeList) {
+  sortList(employeeList);
+  getEmployees(employeeList);
+}
